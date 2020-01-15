520ea31d4cd6533abcd9ff5297ec4368c5c27cba
Sheolis <59622118+Sheolis@users.noreply.github.com>
2020-01-15 15:51:22 +0100

    added cursor pointer on styles choice of guess OPERATIONNAL

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 var secret_pattern=[0];
 var i;
+var color_guess=[0,0,0,0,0,0]; //retiens le choix du joueur quand il appuie sur "test"
+var which_slot;
 
 
 for (var i = 1; i<6;  i++) {
   secret_pattern.push((Math.floor((Math.random() * 8)+1)));
 }
 
-$('.bouton_choix').on('click',function(){
-  $.session.set('selection',this.id[7]);
-  location.reload(true);
+$('.pcolor_spot').on('click',function(){
+  which_slot=this.id[6];
+  color_guess[which_slot]+=1;
+  if (color_guess[which_slot]==9){
+    color_guess[which_slot]=1; //si on est à la dernière couleur on revient à la première.
+    $("#"+this.id).removeClass("color8");
+    $("#"+this.id).addClass("color1");
+  }
+  else {
+    $("#"+this.id).removeClass("color"+(color_guess[which_slot]-1));
+    $("#"+this.id).addClass("color"+color_guess[which_slot]);
+  }
+
 })
