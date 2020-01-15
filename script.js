f26f37ae3722bab8f41ef633c1f9e712d80d8c8c
Sheolis <59622118+Sheolis@users.noreply.github.com>
2020-01-15 17:58:07 +0100

    still have errors

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var secret_pattern=[0];
 var color_guess=[0,0,0,0,0,0]; //retiens le choix du joueur quand il appuie sur "test"
+var no_iteration_check=[0,0,0,0,0,0,0,0,0]; //permet de faire en sorte qu'un indice ne soit pas donné 2 fois pour le même pion
+var quantity_check=0;
 var which_slot;
 var try_number=0;
 var victory_check=0;
@@ -45,17 +47,34 @@ $('.pcolor_spot').mousedown(function(event) {
 $('#validation').on('click',function(){
   try_number++;
   victory_check=0;
+  no_iteration_check=[0,0,0,0,0,0,0,0,0];
   for (var i = 1; i<6; i++) {
     $("#c"+try_number+"_"+i).removeClass("color10");
     $("#c"+try_number+"_"+i).removeClass("color11");
-    $("#"+try_number+"_"+i).addClass("color"+color_guess[i]);
-    if (color_guess[i]==secret_pattern[i]){
-      $("#c"+try_number+"_"+i).addClass("color10");
+    $("#"+try_number+"_"+i).addClass("color"+color_guess[i]);//on met l'historique à jour avec le dernier test
+
+    if (color_guess[i]==secret_pattern[i]){ //si une couleur est bien placée
+      $("#c"+try_number+"_"+i).addClass("color10"); //on le note
       victory_check++;
+      no_iteration_check[color_guess[i]]+=1; //et on retiens qu'un indice à été donné pour cet emplacement.
     }
-    else if (secret_pattern.indexOf(color_guess[i])!=-1){
-      $("#c"+try_number+"_"+i).addClass("color11");
+    else {
       victory_check=0;
+    }
+  }
+  for (var i = 1; i<6; i++) { //on est obligé de faire une deuxieme boucle, afin de placer en priorité les pions qui sont au bon endroit et les comptabiliser
+    if (secret_pattern.indexOf(color_guess[i])!=-1){
+      quantity_check=0;
+      for(var j=1; j<6; j++) {
+        if (secret_pattern[j]==color_guess[i]) {
+          quantity_check++;
+        }
+      }
+      if (no_iteration_check[color_guess[i]]<quantity_check) {
+        $("#c"+try_number+"_"+i).addClass("color11");
+        victory_check=0;
+        no_iteration_check[color_guess[i]]+=1; //et on retiens qu'un indice à été donné pour cet emplacement.
+      }
     }
   }
   if (victory_check==5){
